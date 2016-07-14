76fca356ab1914ca752fbb213cd8540a041ec3ce
Russell Taber <Russell.Taber@target.com>
2016-07-14 16:41:08 -0500

    Basics done

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -11,41 +11,63 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
+
 	final static WikiFetcher wf = new WikiFetcher();
-	
+
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
-	 * 
+	 *
 	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
-	 * 
+	 *
 	 * 1. Clicking on the first non-parenthesized, non-italicized link
      * 2. Ignoring external links, links to the current page, or red links
      * 3. Stopping when reaching "Philosophy", a page with no links or a page
      *    that does not exist, or when a loop occurs
-	 * 
+	 *
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+    ArrayList route = new ArrayList();
+		Set<String> visited = new HashSet<String>();
+		String current_page = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+		while(true) {
+			if current_page.contains("wiki/Philosophy") break;
+
+			route.add(current_page);
+			visited.add(current_page);
+
+			for (Element link: getLinks(current_page)) {
+				String url = link.attr("abs:href");
+				String text = link.text();
+
+				if validLink(current_page, link) {
+					current_page = url;
+				}
 			}
-        }
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+<a href="/w/index.php?title=Parallel_scavenge_garbage_collector&amp;action=edit&amp;redlink=1"
+class="new" title="Parallel scavenge garbage collector (page does not exist)">parallel scavenge garbage collector</a>
+
+		}
+	}
+
+	private Elements getLinks(String url) {
+		Elements paragraphs = wf.fetchWikipedia(url);
+		Elements para_no_italics = paragraphs.select("i").remove();
+		Elements para_no_parens = para_no_italics.toString().replaceAll("\\(.*\\)", "");
+		Document cleaned_pages = Jsoup.parse(para_no_parens);
+		return cleaned_pages.select("a[href]");
+	}
+
+	private boolean validLink(String current_page, String url) {
+		internal = url.contains(wikipedia.org);
+		current = url.contains(current_page);
+		redlink = url.contains("redlink=1");
+		uppercase = Character.isUpperCase(text.charAt(0));
+
+		return internal && !current && !redlink && !uppercase;
 	}
 }
